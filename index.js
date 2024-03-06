b7e7ef32d54249a68e2a76708fa3ed35bc4501b4
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-03-06 17:56:51 -0300

    06032402

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ async function readlExcelFile() {
         totalExpenses: 0
     };
 
+
     worksheet.eachRow((row, rowNumber) => {
         // Suponha que a coluna 2 tem vendas e a coluna 3 tem despesas
         const sales = row.getCell(2).value;
@@ -22,6 +23,7 @@ async function readlExcelFile() {
         processedData.totalExpenses += expenses;
     });
 
+    
     processedData.profit = processedData.totalSales - processedData.totalExpenses;
 
     return processedData;
