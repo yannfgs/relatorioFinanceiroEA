41c8e62be62f4ce090e114802d595cba5d3be29f
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-04-07 23:44:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ async function readlExcelFile() {
         totalSales: 0,
         totalExpenses: 0
     };
+
+  let processedData = {
+        totalSales: 0,
+        totalExpenses: 0
+    };
     
         worksheet.eachRow((row, rowNumber) => {
         // Suponha que a coluna 2 tem vendas e a coluna 3 tem despesas
