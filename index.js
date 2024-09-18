44b1d9f78d5a917f08e7467ed8ffec83ec8948ee
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-09-18 19:09:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ async function readlExcelFile() {
         totalExpenses: 0
     };
 
-    
-            worksheet.eachRow((row, rowNumber) => {
+                worksheet.eachRow((row, rowNumber) => {
         // Suponha que a coluna 2 tem vendas e a coluna 3 tem despesas
         const sales = row.getCell(2).value;
         const expenses = row.getCell(3).value;
